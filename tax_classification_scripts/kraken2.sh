7d8bd5017f5fea2f4543690a09d3bc2f9c37df1b
Cedric TAN <cedritan@pchuckle.cs.ucl.ac.uk>
2024-05-23 14:08:05 +0100

    kraken

--- tax_classification_scripts/kraken2.sh
+++ tax_classification_scripts/kraken2.sh
@@ -3,7 +3,7 @@
 #$ -l h_rt=480:0:0
 #$ -wd /SAN/ugi/HAP_VAP/pneumonia/metagenomic_scripts/logs
 #$ -S /bin/bash
-#$ -pe smp 8
+#$ -pe smp 16
 #$ -j y
 #$ -R y
 #$ -N kraken2
@@ -14,27 +14,27 @@ source /SAN/ballouxlab/uk_bats_meta/miniconda3/etc/profile.d/conda.sh
 conda activate metagenomics
 
 WKDIR=/SAN/ugi/HAP_VAP/pneumonia
-RUN_LIST=$WKDIR/data/raw_fast5s/k2_to_do.txt
 OUTDIR=$WKDIR/results/metagenomic_out/kraken2_out
-FASTQ_DIR=$WKDIR/data/basecalled_fastqs
+FASTQ_DIR=$WKDIR/data/basecalled_fastqs/no_humans
 DB=$WKDIR/databases/k2_pluspf_20231009
 TMP_DIR=$WKDIR/results/metagenomic_out/k2_temp
-N_THREADS=8
+N_THREADS=12
 
 mkdir -p $TMP_DIR
 mkdir -p $OUTDIR
 
 #Classification
-for fq1 in $FASTQ_DIR/INHALE_FRESH_15*.fastq.gz
+for fq1 in $FASTQ_DIR/*.fastq.gz
 do
   prefix=$(echo $fq1|sed "s|$FASTQ_DIR/||g"| sed "s|.fastq.gz||g")
   out_path=$OUTDIR/$prefix.tsv
-
+  echo $prefix
   echo $out_path
 
   kraken2 \
       --threads $N_THREADS\
       --db $DB \
+      --report-minimizer-data \
       --report $out_path \
       $fq1 > /dev/null
 done
